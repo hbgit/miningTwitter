e79de28e5fba45f425a1497a655a76b6f8c0b9a5
Herbert Rocha <herberthb12@gmail.com>
2019-05-24 17:38:12 -0400

    Added stream tuto
    Signed-off-by: Herbert Rocha <herberthb12@gmail.com>

---
+++ stream_tuto/stream.py
@@ -0,0 +1,43 @@
+"""
+Run $python stream.py
+"""
+
+# Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+# Variables that contains the user credentials to access Twitter API
+consumer_key = 'JrbRH8jC5HpyM9UUUdlQ5ughC'
+consumer_secret = 'tBCdiHt04nvoWP5NMm62uJFFYl1L2SUbhvh2Ri7XaGYkyzNewm'
+access_token = '24904823-0rviVLPKT0FzZPXEcvN7igxan5pwS8TlQhXmQH5ON'
+access_token_secret = 'd0jQIhXTneYIczCwcUIApdF3rUMOnpEPYck8LfGjTa8IS'
+
+
+# This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        try:
+            with open('twitter_data.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+if __name__ == '__main__':
+
+    # This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
